a123db019a3edae5f8b6dfdf2b198b17e91bf0c8
ChenJi158 <1582952890@qq.com>
2018-08-04 09:02:47 +0800

    update utils

--- src/net/mcbbs/mineviewer/MineViewer.java
+++ src/net/mcbbs/mineviewer/MineViewer.java
@@ -1,9 +1,25 @@
 package net.mcbbs.mineviewer;
 
+import java.io.File;
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MineViewer extends JavaPlugin{
+public class MineViewer extends JavaPlugin {
+	private static File dataFolder;
+	private static Logger log;
+
 	public void onEnable() {
-		 
+		MineViewer.dataFolder = super.getDataFolder();
+		MineViewer.log = super.getLogger();
 	}
+
+	public static File getKDataFolder() {
+		return MineViewer.dataFolder;
+	}
+
+	public static Logger getLog() {
+		return MineViewer.log;
+	}
+
 }
